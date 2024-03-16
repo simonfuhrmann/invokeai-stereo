f9cefbd9a18af53063739af41593e7e0ce5f43d7
Simon Fuhrmann <simonfuhrmann@gmx.de>
2024-03-16 15:20:24 -0700

    Refactor per-row divergence computation into function

--- stereogram.py
+++ stereogram.py
@@ -10,6 +10,114 @@ from scipy import ndimage
 from PIL import Image
 import numpy as np
 
+
+# This code treats rows of the image as polylines. It generates polylines,
+# applies divergence to them, and then rasterizes them.
+def apply_divergence_row(color_img, depth_img, divergence_px, separation_px, row, derived_img):
+  EPSILON = 1e-7
+  PIXEL_HALF_WIDTH = 0.0   # Sharp vs. smooth (use 0.45 for sharp)
+  STEREO_OFFSET_EXP = 1.0  # Move objects more towards the far plane if >1
+
+  h, w, c = color_img.shape
+  depth_img_row = depth_img[row]
+  color_img_row = color_img[row]
+  derived_img_row = derived_img[row]
+
+  # generating the vertices of the morphed polyline
+  # format: new coordinate of the vertex, divergence (closeness),
+  # column of pixel that contains the point's color
+  pt = np.zeros((5 + 2 * w, 3), dtype=np.float_)
+  pt_end: int = 0
+  pt[pt_end] = [-1.0 * w, 0.0, 0.0]
+  pt_end += 1
+  for col in range(w):
+    coord_d = (depth_img_row[col] ** STEREO_OFFSET_EXP) * divergence_px
+    coord_x = col + 0.5 + coord_d + separation_px
+    if PIXEL_HALF_WIDTH < EPSILON:
+      pt[pt_end] = [coord_x, abs(coord_d), col]
+      pt_end += 1
+    else:
+      pt[pt_end] = [coord_x - PIXEL_HALF_WIDTH, abs(coord_d), col]
+      pt[pt_end + 1] = [coord_x + PIXEL_HALF_WIDTH, abs(coord_d), col]
+      pt_end += 2
+  pt[pt_end] = [2.0 * w, 0.0, w - 1]
+  pt_end += 1
+
+  # Generating the segments of the morphed polyline
+  # Format: coord_x, coord_d, color_i of the first point, then the same for the second point
+  sg_end: int = pt_end - 1
+  sg = np.zeros((sg_end, 6), dtype=np.float_)
+  for i in range(sg_end):
+    sg[i] += np.concatenate((pt[i], pt[i + 1]))
+  # Sort segments and points using insertion sort
+  # Has a very good performance in practice, since these are almost sorted to begin with
+  for i in range(1, sg_end):
+    u = i - 1
+    while pt[u][0] > pt[u + 1][0] and 0 <= u:
+      pt[u], pt[u + 1] = np.copy(pt[u + 1]), np.copy(pt[u])
+      sg[u], sg[u + 1] = np.copy(sg[u + 1]), np.copy(sg[u])
+      u -= 1
+
+  # Rasterizing
+  # At each point in time we keep track of segments that are "active" (or "current")
+  csg = np.zeros((5 * int(abs(divergence_px)) + 25, 6), dtype=np.float_)
+  csg_end: int = 0
+  sg_pointer: int = 0
+  # and index of the point that should be processed next
+  pt_i: int = 0
+  for col in range(w):  # iterate over regions (that will be rasterized into pixels)
+    color = np.full(c, 0.5, dtype=np.float_)  # we start with 0.5 because of how floats are converted to ints
+    while pt[pt_i][0] < col:
+      pt_i += 1
+    pt_i -= 1  # pt_i now points to the dot before the region start
+    # Finding segment' parts that contribute color to the region
+    while pt[pt_i][0] < col + 1:
+      coord_from = max(col, pt[pt_i][0]) + EPSILON
+      coord_to = min(col + 1, pt[pt_i + 1][0]) - EPSILON
+      significance = coord_to - coord_from
+      # the color at center point is the same as the average of color of segment part
+      coord_center = coord_from + 0.5 * significance
+
+      # adding segments that now may contribute
+      while sg_pointer < sg_end and sg[sg_pointer][0] < coord_center:
+        csg[csg_end] = sg[sg_pointer]
+        sg_pointer += 1
+        csg_end += 1
+      # removing segments that will no longer contribute
+      csg_i = 0
+      while csg_i < csg_end:
+        if csg[csg_i][3] < coord_center:
+          csg[csg_i] = csg[csg_end - 1]
+          csg_end -= 1
+        else:
+          csg_i += 1
+      # finding the closest segment (segment with most divergence)
+      # note that this segment will be the closest from coord_from right up to coord_to, since there
+      # no new segments "appearing" inbetween these two and _the polyline does not self-intersect_
+      best_csg_i: int = 0
+      if csg_end != 1:
+        best_csg_closeness: float = -EPSILON
+        for csg_i in range(csg_end):
+          ip_k = (coord_center - csg[csg_i][0]) / (csg[csg_i][3] - csg[csg_i][0])
+          # assert 0.0 <= ip_k <= 1.0
+          closeness = (1.0 - ip_k) * csg[csg_i][1] + ip_k * csg[csg_i][4]
+          if best_csg_closeness < closeness and 0.0 < ip_k < 1.0:
+            best_csg_closeness = closeness
+            best_csg_i = csg_i
+      # getting the color
+      col_l: int = int(csg[best_csg_i][2] + EPSILON)
+      col_r: int = int(csg[best_csg_i][5] + EPSILON)
+      if col_l == col_r:
+        color += color_img_row[col_l] * significance
+      else:
+        ip_k = (coord_center - csg[best_csg_i][0]) / (csg[best_csg_i][3] - csg[best_csg_i][0])
+        color += (color_img_row[col_l] * (1.0 - ip_k) +
+                  color_img_row[col_r] * ip_k
+                  ) * significance
+      pt_i += 1
+    derived_img_row[col] = np.asarray(color, dtype=np.uint8)
+
+
 @invocation("warp3d", title="Warp Image", tags=["image", "infill", "depth", "3d"], category="inpaint", version="1.0.0")
 class WarpImage(BaseInvocation):
   """Warps an image using a depth map and infills the occlusions."""
@@ -19,110 +127,13 @@ class WarpImage(BaseInvocation):
   divergence: int = InputField(default=5, description="The intensity of the 3D effect")
   separation: int = InputField(default=0, description="Amount of image translation")
 
+
   def apply_divergence(self, color_img, depth_img, divergence_px, separation_px):
-    # This code treats rows of the image as polylines. It generates polylines,
-    # applies divergence to them, and then rasterizes them.
-    EPSILON = 1e-7
-    PIXEL_HALF_WIDTH = 0.0   # Sharp vs. smooth (use 0.45 for sharp)
-    STEREO_OFFSET_EXP = 1.0  # Move objects more towards the far plane if >1
-
     h, w, c = color_img.shape
-    derived_image = np.zeros_like(color_img)
+    derived_img = np.zeros_like(color_img)
     for row in range(h):
-      # generating the vertices of the morphed polyline
-      # format: new coordinate of the vertex, divergence (closeness),
-      # column of pixel that contains the point's color
-      pt = np.zeros((5 + 2 * w, 3), dtype=np.float_)
-      pt_end: int = 0
-      pt[pt_end] = [-1.0 * w, 0.0, 0.0]
-      pt_end += 1
-      for col in range(0, w):
-        coord_d = (depth_img[row][col] ** STEREO_OFFSET_EXP) * divergence_px
-        coord_x = col + 0.5 + coord_d + separation_px
-        if PIXEL_HALF_WIDTH < EPSILON:
-          pt[pt_end] = [coord_x, abs(coord_d), col]
-          pt_end += 1
-        else:
-          pt[pt_end] = [coord_x - PIXEL_HALF_WIDTH, abs(coord_d), col]
-          pt[pt_end + 1] = [coord_x + PIXEL_HALF_WIDTH, abs(coord_d), col]
-          pt_end += 2
-      pt[pt_end] = [2.0 * w, 0.0, w - 1]
-      pt_end += 1
-
-      # Generating the segments of the morphed polyline
-      # Format: coord_x, coord_d, color_i of the first point, then the same for the second point
-      sg_end: int = pt_end - 1
-      sg = np.zeros((sg_end, 6), dtype=np.float_)
-      for i in range(sg_end):
-        sg[i] += np.concatenate((pt[i], pt[i + 1]))
-      # Sort segments and points using insertion sort
-      # Has a very good performance in practice, since these are almost sorted to begin with
-      for i in range(1, sg_end):
-        u = i - 1
-        while pt[u][0] > pt[u + 1][0] and 0 <= u:
-          pt[u], pt[u + 1] = np.copy(pt[u + 1]), np.copy(pt[u])
-          sg[u], sg[u + 1] = np.copy(sg[u + 1]), np.copy(sg[u])
-          u -= 1
-
-      # Rasterizing
-      # At each point in time we keep track of segments that are "active" (or "current")
-      csg = np.zeros((5 * int(abs(divergence_px)) + 25, 6), dtype=np.float_)
-      csg_end: int = 0
-      sg_pointer: int = 0
-      # and index of the point that should be processed next
-      pt_i: int = 0
-      for col in range(w):  # iterate over regions (that will be rasterized into pixels)
-        color = np.full(c, 0.5, dtype=np.float_)  # we start with 0.5 because of how floats are converted to ints
-        while pt[pt_i][0] < col:
-          pt_i += 1
-        pt_i -= 1  # pt_i now points to the dot before the region start
-        # Finding segment' parts that contribute color to the region
-        while pt[pt_i][0] < col + 1:
-          coord_from = max(col, pt[pt_i][0]) + EPSILON
-          coord_to = min(col + 1, pt[pt_i + 1][0]) - EPSILON
-          significance = coord_to - coord_from
-          # the color at center point is the same as the average of color of segment part
-          coord_center = coord_from + 0.5 * significance
-
-          # adding segments that now may contribute
-          while sg_pointer < sg_end and sg[sg_pointer][0] < coord_center:
-            csg[csg_end] = sg[sg_pointer]
-            sg_pointer += 1
-            csg_end += 1
-          # removing segments that will no longer contribute
-          csg_i = 0
-          while csg_i < csg_end:
-            if csg[csg_i][3] < coord_center:
-              csg[csg_i] = csg[csg_end - 1]
-              csg_end -= 1
-            else:
-              csg_i += 1
-          # finding the closest segment (segment with most divergence)
-          # note that this segment will be the closest from coord_from right up to coord_to, since there
-          # no new segments "appearing" inbetween these two and _the polyline does not self-intersect_
-          best_csg_i: int = 0
-          if csg_end != 1:
-            best_csg_closeness: float = -EPSILON
-            for csg_i in range(csg_end):
-              ip_k = (coord_center - csg[csg_i][0]) / (csg[csg_i][3] - csg[csg_i][0])
-              # assert 0.0 <= ip_k <= 1.0
-              closeness = (1.0 - ip_k) * csg[csg_i][1] + ip_k * csg[csg_i][4]
-              if best_csg_closeness < closeness and 0.0 < ip_k < 1.0:
-                best_csg_closeness = closeness
-                best_csg_i = csg_i
-          # getting the color
-          col_l: int = int(csg[best_csg_i][2] + EPSILON)
-          col_r: int = int(csg[best_csg_i][5] + EPSILON)
-          if col_l == col_r:
-            color += color_img[row][col_l] * significance
-          else:
-            ip_k = (coord_center - csg[best_csg_i][0]) / (csg[best_csg_i][3] - csg[best_csg_i][0])
-            color += (color_img[row][col_l] * (1.0 - ip_k) +
-                      color_img[row][col_r] * ip_k
-                      ) * significance
-          pt_i += 1
-        derived_image[row][col] = np.asarray(color, dtype=np.uint8)
-    return derived_image
+      apply_divergence_row(color_img, depth_img, divergence_px, separation_px, row, derived_img)
+    return derived_img
 
 
   def warp_image(self, color_img, depth_img, divergence, separation):
@@ -173,7 +184,7 @@ class MakeStereoPair(BaseInvocation):
   """Horizontally stacks a left and right image including a border."""
 
   left: ImageField = InputField(description = "Left image")
-  right: ImageField = InputField(description = "Left image")
+  right: ImageField = InputField(description = "Right image")
   borderPx: int = InputField(default = 8, description = "Pixel size of the border")
   borderColor: ColorField = InputField(
     default = ColorField(r=255, g=255, b=255, a=255),
